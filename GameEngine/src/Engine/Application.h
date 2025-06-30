311fd228f3c5d7f158a45b8439b21c604637ae34
Robert <robertjwisniowski@gmail.com>
2025-06-30 19:23:20 +0200

    Layers were added.

--- GameEngine/src/Engine/Application.h
+++ GameEngine/src/Engine/Application.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "Core.h"
-#include "Events/Event.h"
 #include "Window.h"
-#include "Events/ApplicationEvent.h"
+
+#include "Engine/LayerStack.h"
+#include "Engine/Events/Event.h"
+#include "Engine/Events/ApplicationEvent.h"
 
 namespace Engine {
 
@@ -16,10 +18,14 @@ namespace Engine {
 		void Run();
 
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in CLIENTAdd commentMore actions
--- premake5.lua
+++ premake5.lua
@@ -64,14 +64,17 @@ project "GameEngine"
 
 	filter "configurations:Debug"
 		defines "EG_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "EG_RELEASE"
+		buildoptions "/MD"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "EG_DIST"
+		buildoptions "/MD"
 		optimize "On"
 
 project "Sandbox"
@@ -112,12 +115,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "EG_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "EG_RELEASE"
+		buildoptions "/MD"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "EG_DIST"
+		buildoptions "/MD"
 		optimize "On"
