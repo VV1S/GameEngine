9d764071ac4b6de1beb6767d1155a32af41f4475
Robert <robertjwisniowski@gmail.com>
2025-06-30 18:20:17 +0200

    Window events from GLFW were added

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -7,11 +7,12 @@
 
 namespace Engine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
-		EG_CORE_INFO("Application::Constructor start");
 		m_Window = std::unique_ptr<Window>(Window::Create());
-		EG_CORE_INFO("Application::Constructor end");
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -19,15 +20,27 @@ namespace Engine {
 
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		EG_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
-		EG_CORE_INFO("Application::Run() start");
 		while (m_Running)
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
-		EG_CORE_INFO("Application::Run() end");
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- GameEngine/src/Engine/Application.h
+++ GameEngine/src/Engine/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Engine {
 
@@ -13,7 +14,10 @@ namespace Engine {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
