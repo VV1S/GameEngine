fa75d90dcf780e8d59dd5532a5459ea10f873554
Robert <robertjwisniowski@gmail.com>
2025-07-05 11:36:48 +0200

    ImGui events were added.

--- GameEngine/src/Engine/ImGui/ImGuiLayer.h
+++ GameEngine/src/Engine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Engine/Layer.h"
 
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/KeyEvent.h"
+#include "Engine/Events/MouseEvent.h"
+
 namespace Engine {
 
 	class ENGINE_API ImGuiLayer : public Layer
@@ -14,8 +18,17 @@ namespace Engine {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
-
 }
