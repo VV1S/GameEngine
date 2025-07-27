e3788e7e24dd9e28c7ebe98759e31986841ac9d1
Robert <robertjwisniowski@gmail.com>
2025-07-27 20:24:49 +0200

    OpenGL buffer abstraction was added.

---
+++ GameEngine/src/Engine/Renderer/Buffer.cpp
@@ -0,0 +1,34 @@
+#include "enginepch.h"
+#include "Buffer.h"
+
+#include "Renderer.h"
+
+#include "Platforms/OpenGL/OpenGLBuffer.h"
+
+namespace Engine {
+
+	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::None:    EG_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::OpenGL:  return new OpenGLVertexBuffer(vertices, size);
+		}
+
+		EG_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::None:    EG_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::OpenGL:  return new OpenGLIndexBuffer(indices, size);
+		}
+
+		EG_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
