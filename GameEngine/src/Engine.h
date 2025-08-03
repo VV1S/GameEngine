770a4cb47db465df12a5343a80bcb2c18391a58a
Robert <robertjwisniowski@gmail.com>
2025-08-03 13:48:12 +0200

    Orthographic camera controller was added.

--- GameEngine/src/Engine.h
+++ GameEngine/src/Engine.h
@@ -12,6 +12,7 @@
 #include "Engine/Input.h"
 #include "Engine/KeyCodes.h"
 #include "Engine/MouseButtonCodes.h"
+#include "Engine/OrthographicCameraController.h"
 
 #include "Engine/ImGui/ImGuiLayer.h"
 
